e0be96af468939bb0281014359d187ba9c89ed3e
NabaasaRachael <nabaasarachelle@gmail.com>
2023-02-17 18:52:56 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Desription: all possible combinations of single digits
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+for (a = 0; a < 10; a++)
+{
+	putchar(a + '0');
+	if (a < 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+}
+putchar('\n');
+return (0);
+}
