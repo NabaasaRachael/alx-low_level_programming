db4aaaabc17b0382c667da1ac7eabf9da5f114ee
NabaasaRachael <nabaasarachelle@gmail.com>
2023-03-02 14:15:39 -0800

    5-string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters to uppercase
+ * @s: character to change to upper case
+ *
+ * Return: void
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+	return (s);
+}
