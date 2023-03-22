c8585e4fe2a3be6a5bfffb29487c6aefaced2b64
NabaasaRachael <nabaasarachelle@gmail.com>
2023-03-21 17:09:23 -0700

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+/**
+ * @x: number to check
+ * ABS(x) - a macro that computes the absolute value of a number
+ */
+#define ABS(x) ((x) < 0 ? (-1) * (x) : (x))
+#endif
